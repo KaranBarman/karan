4651449828deeba6fb6988b3616fa4eec405850a
karanbarman <hello.karandev@gmail.com>
2023-12-24 10:06:32 +0530

    added links to project cards

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 const imageStyle = {
   width: "100%",
@@ -9,23 +10,26 @@ interface ProjectCardProps {
   imageSrc: string;
   h3: string;
   p: string;
+  link: string;
 }
-const ProjectCard = ({ imageSrc, h3, p }: ProjectCardProps) => {
+const ProjectCard = ({ imageSrc, h3, p, link }: ProjectCardProps) => {
   return (
     <div className="bg-white rounded-md overflow-hidden">
-      <Image
-        src={imageSrc}
-        style={imageStyle}
-        width={100}
-        height={100}
-        unoptimized
-        className="object-cover"
-        alt="Projects"
-      />
-      <div className="bg-white p-2.5 py-4">
-        <h3 className="font-semibold text-xl">{h3}</h3>
-        <p className="text-sm text-muted-foreground h-16">{p}</p>
-      </div>
+      <Link href={link}>
+        <Image
+          src={imageSrc}
+          style={imageStyle}
+          width={100}
+          height={100}
+          unoptimized
+          className="object-cover"
+          alt="Projects"
+        />
+        <div className="bg-white p-2.5 py-4">
+          <h3 className="font-semibold text-xl">{h3}</h3>
+          <p className="text-sm text-muted-foreground h-16">{p}</p>
+        </div>
+      </Link>
     </div>
   );
 };
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,21 +11,25 @@ const Projects = () => {
       <h2 className="text-2xl font-sans font-semibold text-white">Projects</h2>
       <div className="grid grid-cols-1 gap-1 gap-y-4 lg:grid-cols-3 lg:gap-x-3 xl:grid-cols-4 md:gap-x-2 md:grid-cols-2 my-8">
         <ProjectCard
+          link="https://github.com/KaranBarman/user-management"
           h3="Crud Operations"
           imageSrc="/pro1.jpg"
           p="Next JS 14, Typescript, React Hook Form, Shadcn, Tailwind css"
         />
         <ProjectCard
+          link="https://github.com/KaranBarman/user-management"
           h3="Ecommerce webite"
           p="Next JS 14, Typescript, React Hook Form, Shadcn, Tailwind css"
           imageSrc="/pro2.jpg"
         />
         <ProjectCard
+          link="https://github.com/KaranBarman/user-management"
           h3="Ecommerce webite"
           p="Next JS 14, Typescript, React Hook Form, Shadcn, Tailwind css"
           imageSrc="/pro1.jpg"
         />
         <ProjectCard
+          link="https://github.com/KaranBarman/user-management"
           h3="Ecommerce webite"
           p="Next JS 14, Typescript, React Hook Form, Shadcn, Tailwind css"
           imageSrc="/pro2.jpg"
